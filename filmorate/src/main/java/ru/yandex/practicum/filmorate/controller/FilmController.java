a7cc4a1a762b622029144c40d9c15f688f47e9db
Lsplay2 <lsplaywot8@yandex.ru>
2023-05-11 17:13:08 +0400

    Refact style

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -17,13 +17,14 @@ import java.util.Map;
 public class FilmController {
 
     private final Logger log = LoggerFactory.getLogger(FilmController.class);
-    private final LocalDate MIN_DATE = LocalDate.of(1895,12,28);
+    private final LocalDate minDate = LocalDate.of(1895,12,28);
     private Map<Integer,Film> films = new HashMap<>();
-    private int id=0;
+    private int id = 0;
 
-    private int getId(){
+    private int getId() {
         return ++id;
     }
+
     @GetMapping
     public List<Film> getFilms(){
         return new ArrayList<>(films.values());
@@ -31,31 +32,31 @@ public class FilmController {
 
     @PostMapping
     public Film createFilm(@RequestBody Film film) throws ValidationException {
-        if (film.getName().isEmpty() || film.getDescription().getBytes().length>200
-                || film.getReleaseDate().isBefore(MIN_DATE) || film.getDuration()<0) {
+        if (film.getName().isEmpty() || film.getDescription().getBytes().length > 200
+                || film.getReleaseDate().isBefore(minDate) || film.getDuration() < 0) {
             log.error("Ошибка в одном из полей фильма");
             throw new ValidationException("Ошибка в одном из полей фильма");
         }
         film.setId(getId());
         films.put(film.getId(), film);
-        log.info("Фильм добавлен в коллекцию:"+film);
+        log.info("Фильм добавлен в коллекцию:" + film);
         return film;
     }
 
     @PutMapping
     public Film createOrUpdateFilm(@RequestBody Film film) throws ValidationException {
-        if (film.getName().isEmpty() || film.getDescription().getBytes().length>200
-                || film.getReleaseDate().isBefore(MIN_DATE) || film.getDuration()<0) {
+        if (film.getName().isEmpty() || film.getDescription().getBytes().length > 200
+                || film.getReleaseDate().isBefore(minDate) || film.getDuration() < 0) {
             log.error("Ошибка в одном из полей фильма");
             throw new ValidationException("Ошибка в одном из полей фильма");
         }
 
-        if(!films.containsKey(film.getId())){
+        if (!films.containsKey(film.getId())) {
             log.error("Ошибка фильм с таким id не создан");
             throw new ValidationException("Фильма с таким id не существует");
         }
         films.put(film.getId(), film);
-        log.info("Фильм обновлен в коллекции:"+film);
+        log.info("Фильм обновлен в коллекции:" + film);
         return film;
     }
 
