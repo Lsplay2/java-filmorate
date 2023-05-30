42ac4459ed6fde1bb8d442f5c106f3fa21c13035
Lsplay2 <lsplaywot8@yandex.ru>
2023-05-30 20:11:24 +0400

    Remaking Style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class NotFoundException extends Exception{
-    public NotFoundException (String e) {
+public class NotFoundException extends Exception {
+    public NotFoundException(String e) {
         super(e);
     }
 }
