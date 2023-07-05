2bd2c526c88769d1db48d39c01f5ed159b2302f7
Kirill Zykov <kompas009@yandex.ru>
2023-07-05 21:03:51 +0300

    FIX: Добавление функционала "Рекомендации фильмов" 3

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
